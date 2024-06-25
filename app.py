bfff9fed4d662dddc2d9cc7df2d357c4309670ad
vinay <vinaymahto999@gmail.com>
2024-06-25 15:03:23 +0530

    email_send_at_12clock

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask, jsonify, request, abort, render_template
 from flask_sqlalchemy import SQLAlchemy
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask_mail import Mail, Message
 import pymysql
 import datetime
+import json
 
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost:3306/new_email"
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'  
+app.config['MAIL_PORT'] = 587  
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'vinaykumar900417@gmail.com'  
+app.config['MAIL_PASSWORD'] = 'fgyc cjhy lfmb fddk'  
+app.config['MAIL_DEFAULT_SENDER'] = 'vinaykumar900417@gmail.com'  
+
 db = SQLAlchemy(app)
+mail = Mail(app)
+scheduler = BackgroundScheduler()
 
 # Define SQLAlchemy model
 class Question1(db.Model):
@@ -40,9 +53,7 @@ def page_not_found(e):
 def internal_server_error(e):
     return jsonify(error=str(e)), 500
 
-# Routes
 
-# Create a question
 @app.route('/')
 def survey_form():
     question = Question1.query.first()
@@ -70,6 +81,34 @@ def submit_answer():
 
     return render_template('submit.html')
 
-# Run the Flask application
+def send_email():
+    with app.app_context():
+        questions = Question1.query.all()
+
+        
+        html_content = "<html><body>"
+        html_content += "<h2>Survey Results</h2>"
+        
+        for question in questions:
+            html_content += f"<h3>{question.text}</h3>"
+            html_content += "<ul>"
+            html_content += f"<li>{question.option1} - Selected {question.option1_selected_count} times</li>"
+            html_content += f"<li>{question.option2} - Selected {question.option2_selected_count} times</li>"
+            html_content += f"<li>{question.option3} - Selected {question.option3_selected_count} times</li>"
+            html_content += "</ul>"
+        
+        html_content += "</body></html>"
+
+        msg = Message("Survey Results", recipients=['pradeep@maitri.nyc'])  # Replace with recipient's email
+        msg.html = html_content
+        mail.send(msg)
+        print("Email sent!")
+
+
+
+scheduler.add_job(send_email, 'cron', hour=15, minute=5)
+scheduler.start()
+
+
 if __name__ == '__main__':
     app.run(debug=True)
